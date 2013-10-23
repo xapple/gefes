707d8cb07c2c381f95fc57204ed13cd1cb8e1b70
Ino de Bruijn <ino.debruijn@scilifelab.se>
2013-10-23 14:41:28 +0200

    Init mapper

--- gefes/helper/mapper.py
+++ gefes/helper/mapper.py
@@ -2,25 +2,47 @@
 from __future__ import division
 
 # Built-in modules #
+import os
 
 # Internal modules #
-from gefes.common import AutoPaths
+from gefes.common.autopaths import AutoPaths
 
 # Third party modules #
+import sh
 
 ###############################################################################
 class Mapper(object):
     """Lorem ipsum."""
 
     all_paths = """
-    /lorem.txt
+    /map/map.sam
+    /map/map.bam
     """
 
     def __repr__(self): return '<%s object of %s>' % (self.__class__.__name__, self.parent)
 
-    def __init__(self, pool):
-        # Save parent #
+    def __init__(self, pool, assembly):
+        # Save parents #
         self.parent, self.pool = pool, pool
+        self.assembly = assembly
         # Auto paths #
-        self.base_dir = self.parent.base_dir
+        self.base_dir = self.assembly.base_dir + self.pool.id_name + '/'
         self.p = AutoPaths(self.base_dir, self.all_paths)
+        # Convenience objects #
+        self.fwd = self.pool.fwd
+        self.rev = self.pool.rev
+
+    def map(self):
+        # Create bowtie2 assembly index #
+        ref = self.assembly.contigs_fasta.path
+
+        if not os.path.exists(ref + ".1.bt2"):
+            sh.bowtie2_build(ref, ref)
+        nr_threads = 1
+        sh.bowtie2('-p', nr_threads, '-x', ref, '-1', self.fwd.path, '-2', self.rev.path, '-S', self.p.sam)
+
+        # Create samtools assembly index #
+        if not os.path.exists(self.assembly.contigs_fasta.path + ".fai"):
+            sh.samtools('faidx', self.assembly.contigs_fasta.path)
+
+        sh.samtools('view', '-bt', self.assembly.contigs_fasta.path + '.fai', self.p.sam, _out=self.p.bam)
