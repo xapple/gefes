c745bda7469e0336fbbaf8c50442353428ddcea6
moritzbuck <mrtz.buck@gmail.com>
2014-01-09 20:00:22 +0100

    Bunched tetramers with rev-comps, and so on. pfiou... kept on loosing tetramers stupidly... now sum of all is one (plus/minus floating-point error) for all contigs

--- gefes/helper/binner.py
+++ gefes/helper/binner.py
@@ -13,6 +13,8 @@ from gefes.common.cache import property_cached
 import pandas
 from sklearn import cluster
 from Bio.Seq import Seq
+from scipy.spatial import distance
+from scipy.cluster import hierarchy
 
 ###############################################################################
 class Binner(object):
@@ -41,12 +43,22 @@ class Binner(object):
     def frame(self):
         tetramers = ["".join(tetramer) for tetramer in product('ACGT', repeat=4)]
         for t in tetramers:
-            tetramers.remove(Seq(t).reverse_complement().tostring())
+            if Seq(t).reverse_complement().tostring() != t:
+                tetramers.remove(Seq(t).reverse_complement().tostring())
+        for t in tetramers:
+            if t[::-1] in tetramers:
+                if t!=t[::-1]:
+                    tetramers.remove(t[::-1])
 
+        tetra_cats={t:list(set((t,t[::-1],Seq(t).reverse_complement().tostring(),Seq(t[::-1]).reverse_complement().tostring()))) for t in tetramers}
+        
         columns = ['length'] + [s.id_name for s in self.aggregate] + ['freq_' + t for t in tetramers]
         rows = [c.name for c in self.aggregate.assembly.contigs]
         data = [[c.length] +
                 [s.mapper.coverage[c.name]["cov_mean"] for s in self.aggregate] +
-                [c.tetra_nuc_freq.get(t, 0) for t in tetramers] for c in self.aggregate.assembly.contigs]
+                [sum([c.tetra_nuc_freq.get(tt,0) for tt in tetra_cats[t]]) for t in tetra_cats] for c in self.aggregate.assembly.contigs]
         return pandas.DataFrame(data, columns=columns, index=rows)
+
+    def export_frame(self):
+        self.frame.to_csv(self.p.frame)
 
