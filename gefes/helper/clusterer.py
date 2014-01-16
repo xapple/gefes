9e0e79b8e5f708d20ce86517e1b305714c551d54
moritzbuck <mrtz.buck@gmail.com>
2014-01-16 10:49:41 +0100

    put properties and made the parameter adjustments more robust, looks quite good I find

--- gefes/helper/clusterer.py
+++ gefes/helper/clusterer.py
@@ -32,8 +32,8 @@ class Clusterer(object):
         self.base_dir = self.parent.p.clustering
         self.p = AutoPaths(self.base_dir, self.all_paths)
         self.kmeans = GefesKMeans(self)
-        self.min_length = 0;
-        self.max_freq = 0;
+        self.__min_length = 0
+        self.__max_freq = 0
         
     def run(self):
         pass
@@ -41,7 +41,7 @@ class Clusterer(object):
     def frame_filter(self):
         temp_frame = self.parent.frame
         if(self.min_length):
-            temp_frame = temp_frame[temp_frame.length > self.min_length]
+            temp_frame = temp_frame[temp_frame.length > self.__min_length]
         if(self.max_freq):
             good_ones = temp_frame[[c for c in temp_frame if "freq" in c]].apply(lambda x: sum(x>self.max_freq)!=0,1)
             temp_frame = temp_frame[good_ones]
@@ -50,27 +50,67 @@ class Clusterer(object):
         names = [f[0] for f in temp_frame.itertuples()]
         return (names,tetras,covers)
 
+        
+    @property
+    def min_length(self):
+        return self.__min_length
+        
+    @property
+    def max_freq(self):
+        return self.__max_freq
+
+    @min_length.setter
+    def min_length(self,len):
+        __min_length=len
+        self.kmeans.reset()
+
+    @max_freq.setter
+    def max_freq(self,freq):
+        __max_freq=freq
+        self.kmeans.reset()
+
     
+        
+        
 ###############################################################################
 class GefesKMeans(object):
     """Receives the matrix and uses kmeans to cluster it in N different (linearly separated) clusters"""
 
-    def __init__(self,parent,number_clusts=8):
-        self.number_clusts=number_clusts
+    def __init__(self,parent,nb=8):
+        self.__number_clusts=nb
         self.parent = parent
+        self.algorithm = KMeans(self.__number_clusts)
+        self.__tetras_clusters = None
+        self.__coverage_clusters = None
+        
 
-    @property_cached
-    def algorithm(self):
-        return KMeans(self.number_clusts)
+    def run(self):
+        (names,tetras,covers) = self.parent.frame_filter()
+        self.__tetras_clusters = self.algorithm.fit_predict(tetras)        
+        self.__coverage_clusters = self.algorithm.fit_predict(covers)
 
-    @property_cached
+    def reset(self):
+        self.__tetras_clusters = None
+        self.__coverage_clusters = None
+        
+    @property
+    def number_clusts(self):
+        return __number_clusts
+
+    @number_clusts.setter
+    def number_clusts(self,nb):
+        self.__number_clusts = nb
+        self.algorithm = KMeans(self.__number_clusts)
+        self.reset()
+        
+    @property
     def tetras_clusters(self):
-        (names,tetras,covers) = self.parent.frame_filter()
-        return self.algorithm.fit_predict(tetras)
+        if(self.__tetras_clusters is None): self.run()       
+        return self.__tetras_clusters
 
-    @property_cached
+    @property
     def coverage_clusters(self):
-        (names,tetras,covers) = self.parent.frame_filter()
-        return self.algorithm.fit_predict(covers)
+        if(self.__coverage_clusters is None): self.run()       
+        return  self.__coverage_clusters
 
     
