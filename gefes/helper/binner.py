edd165f503cb41e2749f0be9cf93386c08214cce
moritzbuck <mrtz.buck@gmail.com>
2014-03-10 17:25:44 +0100

    made a good start refactoring, and cleaning up the binning, so I can include the gene-finding and annotation part

--- gefes/helper/binner.py
+++ gefes/helper/binner.py
@@ -3,6 +3,7 @@ from __future__ import division
 
 # Built-in modules #
 from math import log10
+import os
 
 # Internal modules #
 from gefes.common.autopaths import AutoPaths
@@ -10,6 +11,7 @@ from gefes.helper.clusterer import Clusterer
 from gefes.common.cache import property_cached
 from gefes.fasta.single import FASTA
 from gefes.helper.contig import Contig
+from gefes.common.autopaths import AutoPaths
 
 # Third party modules #
 from pandas import DataFrame
@@ -20,40 +22,81 @@ class Binner(object):
     The output is N bins, each with one or more contigs inside."""
 
     all_paths = """
-    /clustering/
-    /bins/
-    /frame.csv
     """
 
-    def __repr__(self): return '<%s object of %s>' % (self.__class__.__name__, self.parent)
-
-    def __init__(self, aggregate):
+    def __repr__(self): return '<%s object of %s with %i Binnings>' % \
+                               (self.__class__.__name__, self.parent, len(self))
+    def __iter__(self): return iter(self.binnings)
+    def __len__(self): return len(self.binnings)
+    def __getitem__(self, key):
+        return self.binnings[key]
+            
+    def __init__(self, parent):
         # Save parent #
-        self.parent, self.aggregate = aggregate, aggregate
+        self.parent = parent
+        self.assembly = parent.assembly
         # Auto paths #
         self.base_dir = self.parent.p.binning_dir
         self.p = AutoPaths(self.base_dir, self.all_paths)
         # Children #
-        self.clusterer=Clusterer(self)
-        # Output #
-        self.bins = []
-        self.bins_clusterer = None
+        self.binnings=self.load()
 
+
+    def load(self):
+        binnings={}
+        binnings_paths = os.listdir(self.base_dir)
+        for b in binnings_paths:
+            print b
+            binnings[b]=Binning(self, b)
+        return binnings
+
+    def new(self,name,clusterer):
+        self.binnings[name] = Binning(self, name, clusterer)
+             
+
+###############################################################################        
+class Binning(object):
+
+    all_paths = """
+    /bins/
+    /settings.json
+    /linkage_quality.csv
+    /linkage_matrix.csv
+    /coverage_stats.csv
+    /frame.csv
+    """
+
+    def __init__(self, parent,name ,clusterer = None):
+        # Save parent #
+        self.parent =  parent
+        self.name = name
+        # Auto paths #
+        self.base_dir = self.parent.p._base_dir+name
+        self.p = AutoPaths(self.base_dir, self.all_paths)
+        # Output #
+        self.clusterer = clusterer
+        if clusterer is None: self.load()
+
+    def load(self):
+        pass
+    
+            
     def run(self):
-        if self.clusterer.clusters is None: self.clusterer.run()
+        if self.clusterer is None: raise Exception('Clustering has already been run, or is broken, or not existing')
+        self.clusterer.run(self.parent.assembly)
         c_ids=list(set([c[1] for c in self.clusterer.clusters]))
         self.bins = dict.fromkeys(c_ids)
         for k in self.bins: self.bins[k] = []
         for co,cl in self.clusterer.clusters:
             self.bins[cl].append(co)
-        self.bins_clusterer = self.clusterer.default
-
-
+        self.frame=self.clusterer.frame
+        self.export()
+            
     def linkage_quality(self):
         linkage=[p.mapper.linkage for p in self.aggregate]
         
         qualities = {}
-        all_contigs = list(self.bins_clusterer.frame.index)
+        all_contigs = list(self.frame.index)
         
         for k,b in self.bins.iteritems():
             qualities[k] = [0,0,0,0]
@@ -90,13 +133,17 @@ class Binner(object):
                                     matrix[k1][k2] = matrix[k1][k2] + sum(p[contig1][contig2])/2.0
                                     matrix[k2][k1] = matrix[k2][k1] + sum(p[contig1][contig2])/2.0
         return matrix
-    
-
-    
                     
     def bins_stats(self):
         out={}
         for k,b in self.bins.iteritems():
-            out[k]=self.bins_clusterer.frame[[c for c in self.bins_clusterer.frame if "pool" in c]].loc[b].applymap(lambda x: log10(1+x))
+            out[k]=self.frame[[c for c in self.frame if "pool" in c]].loc[b].applymap(lambda x: log10(1+x))
             out[k]=out[k].median()
         return DataFrame.from_dict(out)
+
+
+    def export(self):
+        path=self.p.bins
+        for name,contig_list in self.bins.iteritems():
+            self.parent.assembly.write_contiglist(contig_list,path,"bin_"+str(name)+".fasta")
+
--- gefes/helper/clusterer.py
+++ gefes/helper/clusterer.py
@@ -4,7 +4,6 @@ from __future__ import division
 # Built-in modules #
 
 # Internal modules #
-from gefes.common.autopaths import AutoPaths
 from gefes.common.cache import property_cached
 
 # Third party modules #
@@ -20,32 +19,13 @@ import math
 
 ###############################################################################
 class Clusterer(object):
-    """Recieves the matrix detailing all information for every contig,
-    and is responsible for deciding which contigs go together."""
-
-    all_paths = """
-    /lorem.txt
-    /kmeans/
-    """
+    """Recieves an assembly, outputs a clustering"""
 
     def __repr__(self): return '<%s object of %s>' % (self.__class__.__name__, self.parent)
 
-    def __init__(self, binner):
-        # Save parent #
-        self.parent = binner
-        self.assembly = self.parent.aggregate.assembly
-        # Auto paths #
-        self.base_dir = self.parent.p.clustering
-        self.p = AutoPaths(self.base_dir, self.all_paths)
-        # Methods
-        self.kmeans = GefesKMeans(self)
-        self.default = self.kmeans
+    def __init__(self):
         self.clusters = None
-        
-    def run(self):
-        self.kmeans.run()
-        self.clusters=self.kmeans.clusters
-        
+                
     def log10(x):
         return x.applymap(math.log10)
 
@@ -60,79 +40,32 @@ class Clusterer(object):
     
         
 ###############################################################################
-class GefesKMeans(object):
+class GefesKMeans(Clusterer):
     """Receives the matrix and uses kmeans to cluster it in N different (linearly separated) clusters"""
-
-    all_paths = """
-    /tetramer_clusters/
-    /coverage_clusters/
-    /combo_clusters/
-    """
-
     
-    def __init__(self,parent,nb = 8,max_freq = None,min_length = None, transform = None):
+    def __init__(self,nb = 8,method = 'tetramer', max_freq = None,min_length = None, transform = None):
         # Save parent
-        self.parent = parent
+        super(Clusterer,self).__init__()
         # Kmeans and params
         self.number_clusts=nb
         self.algorithm = KMeans(self.number_clusts)
-        # Autopath
-        self.base_dir = self.parent.p.kmeans
-        self.p = AutoPaths(self.base_dir, self.all_paths)
-        # Clusters
-        self.tetras_clusters = None
-        self.coverage_clusters = None
-        self.combo_clusters = None
         # Filters
+        self.method = method
         self.max_freq = max_freq
         self.min_length = min_length
-        self.transform = None
-        self.clusters = None
-        
+        self.transform = transform
 
-    def run(self, nb = None, max_freq = None, min_len = None, transform = None):
-        if nb:
-            self.number_clusts=nb
-            self.algorithm = KMeans(self.number_clusts)
-        if max_freq: self.max_freq = max_freq
-        if min_len: self.min_length = min_len
-        if transform: self.transform = transform
-        self.frame = self.parent.assembly.filtered_frame(self.max_freq,self.min_length)
-        tetras = self.frame[[c for c in self.frame if "freq" in c]]
-        covers = self.frame[[c for c in self.frame if "pool" in c]]
-        combo = self.frame[[c for c in self.frame if "freq" in c or "pool" in c]]
-        if self.transform: covers = Clusterer.transforms[self.transform](covers)
-        if self.transform: tetras = Clusterer.transforms[self.transform](tetras)
-        if self.transform: combo = Clusterer.transforms[self.transform](combo)
-        self.tetras_clusters = self.algorithm.fit_predict(tetras)        
-        self.coverage_clusters = self.algorithm.fit_predict(covers)
-        self.combo_clusters = self.algorithm.fit_predict(combo)
-        self.clusters =  zip(self.frame.index,self.tetras_clusters)
-        self.parent.clusters = self.clusters
-        self.parent.default =  self
+    def run(self,assembly):
+        self.frame = assembly.filtered_frame(self.max_freq,self.min_length)
+        if self.method == 'tetramer':
+            data = self.frame[[c for c in self.frame if "freq" in c]]
+        elif self.method == 'coverage':
+            data = self.frame[[c for c in self.frame if "pool" in c]]
+        elif self.method == 'uwcombo':
+            data = self.frame[[c for c in self.frame if "freq" in c or "pool" in c]]
+        else: raise Exception("This method does not exist in da Kmeans")
+                
+        if self.transform: data = Clusterer.transforms[self.transform](data)
 
-                #self.export()
-
-    def export(self):
-        parameters = "-".join(["nb_clusts",str(self.number_clusts),"min_len",str(self.min_length),"max_freq",str(self.max_freq),"transform",self.transform])
-        assembly = self.parent.parent.parent.assembly
-        contigs = [f[0] for f in self.frame.itertuples()]
-        path=os.path.join(self.p.tetramer,parameters)
-        for i in set(self.tetras_clusters):
-            contig_list=[]
-            for j in range(0,len(contigs)):
-                if(self.tetras_clusters[j] == i): contig_list.append(contigs[j])
-            assembly.write_contiglist(contig_list,path,"bin_"+str(i)+".fasta")
-        path=os.path.join(self.p.coverage,parameters)
-        for i in set(self.coverage_clusters):
-            contig_list=[]
-            for j in range(0,len(contigs)):
-                if(self.coverage_clusters[j] == i): contig_list.append(contigs[j])
-            assembly.write_contiglist(contig_list,path,"bin_"+str(i)+".fasta")
-        path=os.path.join(self.p.combo,parameters)
-        for i in set(self.combo_clusters):
-            contig_list=[]
-            for j in range(0,len(contigs)):
-                if(self.combo_clusters[j] == i): contig_list.append(contigs[j])
-            assembly.write_contiglist(contig_list,path,"bin_"+str(i)+".fasta")
+        self.clusters = zip(self.frame.index,self.algorithm.fit_predict(data))
 
