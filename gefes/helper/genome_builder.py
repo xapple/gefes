644e7cbf652ac83474df4863ae17b9a56c195f89
moritzbuck <mrtz.buck@gmail.com>
2014-05-30 18:05:17 +0200

    started work on a genome builer-reassembler

---
+++ gefes/helper/genome_builder.py
@@ -0,0 +1,88 @@
+
+#built-in modules#
+import os
+
+# Internal modules #
+from gefes.common.autopaths import AutoPaths
+from gefes.common.slurm import nr_threads
+from gefes.common import flatten
+from gefes.fasta.single import FASTA
+
+# Third party mods #
+import sh
+from numpy import array
+import numpy
+from matplotlib import cm
+from matplotlib import pyplot
+from pandas import DataFrame
+from sklearn import mixture
+
+class GenomeBuilder(object):
+    """reassembling a genome from a bins and the reads"""
+
+    all_paths = """
+    /scaffolds.fasta
+    /filtered.scaffold.fasta
+    /contigs
+    /graph_contigs.pdf
+    /reads.fastq
+    /reads.1.fastq
+    /reads.2.fastq
+    /bin_vs_reassembled.png
+    """
+
+    def __init__(self,parent):
+        self.parent = parent
+        self.bini = parent
+        self.fwds=[str(p.cleaner.fwd.path) for p in self.bini.parent.parent.parent]
+        self.revs=[str(p.cleaner.rev.path) for p in self.bini.parent.parent.parent]
+        # Auto paths #
+        self.base_dir = self.parent.p.rebuilt
+        self.p = AutoPaths(self.base_dir, self.all_paths)
+
+    
+    def pull_reads(self):
+        sh.bowtie2_build(self.parent.p.contigs,self.p.contigs)
+        sh.bowtie2("-p", nr_threads, "-x",self.p.contigs,"-1", ",".join(self.fwds), "-2", ",".join(self.revs), "--al-conc", self.p.fastq, "-S", "/dev/null")
+
+    def assemble_genome(self):
+        spades_script = sh.Command("spades.py")
+        spades_script("-o", self.base_dir,  "-1", self.p.reads_1, "-2", self.p.reads_2,  "-t", nr_threads, "--careful")
+
+    def filter_assembly(self,cutoff=0.3):
+        original = FASTA(self.p.scaffold)
+        data = DataFrame.from_dict({s.id : array(s.id.split("_"))[[3,5]] for s in original}, orient='index')
+        data = data.astype(numpy.float)
+        previous_min = array([c.length for c in self.parent.contigs]).min()
+        data = data[data[0] > prev_min]
+        classes = clust(numpy.log10(data))
+        lens = data[0].groupby(classes).apply(sum)
+        data[classes == lens.idxmax()]
+        keepers = data[classes == lens.idxmax()].index
+        [s for s in original if s in keepers]
+        with FASTA(self.p.filtered) as filtered:
+            filtered.add_seq([s for s in original if s.id in keepers])
+            
+    def clust(data):
+        gmm=mixture.GMM(n_components=2)
+        gmm.fit(data[0])
+        clust_x = gmm.predict(data[0])
+        gmm.fit(data[1])
+        clust_y = gmm.predict(data[1])
+        return clust_x + 2*clust_y
+
+    def contig_plot(self,data,classes):
+        fig = pyplot.figure()
+        axes = fig.add_subplot(111)
+        axes.set_xscale('log')
+        axes.set_yscale('log')
+        pyplot.scatter(data[0],data[1],c=classes,cmap=cm.rainbow)
+        fig.savefig(self.p.graph)
+            
+    def graphs(self):
+        return 0
+        #        nucmer -l 1000  scaffolds.fasta ../../thorsellia_type.fasta
+#        mummerplot --layout --png --large -R scaffolds.fasta -Q ../../thorsellia_type.fasta  out.delta
+#        gnuplot out.gp > out.png
+
+
