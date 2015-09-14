28ae0e7b69b7142afef3dddfc76b02cc5aa61b8f
Lucas Sinclair <lucas.sinclair@me.com>
2015-09-14 10:53:27 +0200

    added the omnigraffle files

---
+++ scripts/projects/soda_eval/copy_reports.sh
@@ -0,0 +1,15 @@
+rsync -av lsinclai@sisu-login1.csc.fi:/wrk/eiler/GEFES/views/projects/soda_eval/assembly/ray/51/report/report.pdf ~/GEFES/reports/soda_eval/ray_51.pdf
+rsync -av lsinclai@sisu-login1.csc.fi:/wrk/eiler/GEFES/views/projects/soda_eval/assembly/ray/61/report/report.pdf ~/GEFES/reports/soda_eval/ray_61.pdf
+rsync -av lsinclai@sisu-login1.csc.fi:/wrk/eiler/GEFES/views/projects/soda_eval/assembly/ray/71/report/report.pdf ~/GEFES/reports/soda_eval/ray_71.pdf
+rsync -av lsinclai@sisu-login1.csc.fi:/wrk/eiler/GEFES/views/projects/soda_eval/assembly/ray/81/report/report.pdf ~/GEFES/reports/soda_eval/ray_81.pdf
+rsync -av lsinclai@sisu-login1.csc.fi:/wrk/eiler/GEFES/views/projects/soda_eval/merged/newbler/report/report.pdf  ~/GEFES/reports/soda_eval/merged.pdf
+
+mkdir -p ~/GEFES/reports/soda_eval/
+rsync -av lsinclai@sisu-login1.csc.fi:/wrk/eiler/GEFES/views/projects/soda_eval/samples/ss01/report/report.pdf ~/GEFES/reports/soda_eval/ss01.pdf
+rsync -av lsinclai@sisu-login1.csc.fi:/wrk/eiler/GEFES/views/projects/soda_eval/samples/ss04/report/report.pdf ~/GEFES/reports/soda_eval/ss04.pdf
+rsync -av lsinclai@sisu-login1.csc.fi:/wrk/eiler/GEFES/views/projects/soda_eval/samples/ss10/report/report.pdf ~/GEFES/reports/soda_eval/ss10.pdf
+rsync -av lsinclai@sisu-login1.csc.fi:/wrk/eiler/GEFES/views/projects/soda_eval/samples/ss12/report/report.pdf ~/GEFES/reports/soda_eval/ss12.pdf
+rsync -av lsinclai@sisu-login1.csc.fi:/wrk/eiler/GEFES/views/projects/soda_eval/samples/zl01/report/report.pdf ~/GEFES/reports/soda_eval/zl01.pdf
+rsync -av lsinclai@sisu-login1.csc.fi:/wrk/eiler/GEFES/views/projects/soda_eval/samples/zl02/report/report.pdf ~/GEFES/reports/soda_eval/zl02.pdf
+rsync -av lsinclai@sisu-login1.csc.fi:/wrk/eiler/GEFES/views/projects/soda_eval/samples/zl12/report/report.pdf ~/GEFES/reports/soda_eval/zl12.pdf
+rsync -av lsinclai@sisu-login1.csc.fi:/wrk/eiler/GEFES/views/projects/soda_eval/samples/zl24/report/report.pdf ~/GEFES/reports/soda_eval/zl24.pdf
---
+++ scripts/projects/under_ice/copy_reports.sh
@@ -0,0 +1,47 @@
+mkdir -p ~/GEFES/reports/under_ice_bt/
+rsync -av lsinclai@sisu-login1.csc.fi:/wrk/eiler/GEFES/views/projects/under_ice_bt/assembly/ray/51/report/report.pdf ~/GEFES/reports/under_ice_bt/ray_51.pdf
+rsync -av lsinclai@sisu-login1.csc.fi:/wrk/eiler/GEFES/views/projects/under_ice_bt/assembly/ray/61/report/report.pdf ~/GEFES/reports/under_ice_bt/ray_61.pdf
+rsync -av lsinclai@sisu-login1.csc.fi:/wrk/eiler/GEFES/views/projects/under_ice_bt/assembly/ray/71/report/report.pdf ~/GEFES/reports/under_ice_bt/ray_71.pdf
+rsync -av lsinclai@sisu-login1.csc.fi:/wrk/eiler/GEFES/views/projects/under_ice_bt/assembly/ray/81/report/report.pdf ~/GEFES/reports/under_ice_bt/ray_81.pdf
+rsync -av lsinclai@sisu-login1.csc.fi:/wrk/eiler/GEFES/views/projects/under_ice_bt/merged/newbler/report/report.pdf ~/GEFES/reports/under_ice_bt/merged.pdf
+
+mkdir -p ~/GEFES/reports/under_ice_lb/
+rsync -av lsinclai@sisu-login1.csc.fi:/wrk/eiler/GEFES/views/projects/under_ice_lb/assembly/ray/51/report/report.pdf ~/GEFES/reports/under_ice_lb/ray_51.pdf
+rsync -av lsinclai@sisu-login1.csc.fi:/wrk/eiler/GEFES/views/projects/under_ice_lb/assembly/ray/61/report/report.pdf ~/GEFES/reports/under_ice_lb/ray_61.pdf
+rsync -av lsinclai@sisu-login1.csc.fi:/wrk/eiler/GEFES/views/projects/under_ice_lb/assembly/ray/71/report/report.pdf ~/GEFES/reports/under_ice_lb/ray_71.pdf
+rsync -av lsinclai@sisu-login1.csc.fi:/wrk/eiler/GEFES/views/projects/under_ice_lb/assembly/ray/81/report/report.pdf ~/GEFES/reports/under_ice_lb/ray_81.pdf
+rsync -av lsinclai@sisu-login1.csc.fi:/wrk/eiler/GEFES/views/projects/under_ice_lb/merged/newbler/report/report.pdf  ~/GEFES/reports/under_ice_lb/merged.pdf
+
+mkdir -p ~/GEFES/reports/under_ice_kt/
+rsync -av lsinclai@sisu-login1.csc.fi:/wrk/eiler/GEFES/views/projects/under_ice_kt/assembly/ray/51/report/report.pdf ~/GEFES/reports/under_ice_kt/ray_51.pdf
+rsync -av lsinclai@sisu-login1.csc.fi:/wrk/eiler/GEFES/views/projects/under_ice_kt/assembly/ray/61/report/report.pdf ~/GEFES/reports/under_ice_kt/ray_61.pdf
+rsync -av lsinclai@sisu-login1.csc.fi:/wrk/eiler/GEFES/views/projects/under_ice_kt/assembly/ray/71/report/report.pdf ~/GEFES/reports/under_ice_kt/ray_71.pdf
+rsync -av lsinclai@sisu-login1.csc.fi:/wrk/eiler/GEFES/views/projects/under_ice_kt/assembly/ray/81/report/report.pdf ~/GEFES/reports/under_ice_kt/ray_81.pdf
+rsync -av lsinclai@sisu-login1.csc.fi:/wrk/eiler/GEFES/views/projects/under_ice_kt/merged/newbler/report/report.pdf  ~/GEFES/reports/under_ice_kt/merged.pdf
+
+rsync -av lsinclai@sisu-login1.csc.fi:/wrk/eiler/GEFES/views/projects/under_ice_bt/samples/bt1/report/report.pdf ~/GEFES/reports/under_ice_bt/bt1.pdf
+rsync -av lsinclai@sisu-login1.csc.fi:/wrk/eiler/GEFES/views/projects/under_ice_bt/samples/bt2/report/report.pdf ~/GEFES/reports/under_ice_bt/bt2.pdf
+rsync -av lsinclai@sisu-login1.csc.fi:/wrk/eiler/GEFES/views/projects/under_ice_bt/samples/bt3/report/report.pdf ~/GEFES/reports/under_ice_bt/bt3.pdf
+rsync -av lsinclai@sisu-login1.csc.fi:/wrk/eiler/GEFES/views/projects/under_ice_bt/samples/bt4/report/report.pdf ~/GEFES/reports/under_ice_bt/bt4.pdf
+rsync -av lsinclai@sisu-login1.csc.fi:/wrk/eiler/GEFES/views/projects/under_ice_bt/samples/bt5/report/report.pdf ~/GEFES/reports/under_ice_bt/bt5.pdf
+rsync -av lsinclai@sisu-login1.csc.fi:/wrk/eiler/GEFES/views/projects/under_ice_bt/samples/bt6/report/report.pdf ~/GEFES/reports/under_ice_bt/bt6.pdf
+rsync -av lsinclai@sisu-login1.csc.fi:/wrk/eiler/GEFES/views/projects/under_ice_bt/samples/bt7/report/report.pdf ~/GEFES/reports/under_ice_bt/bt7.pdf
+rsync -av lsinclai@sisu-login1.csc.fi:/wrk/eiler/GEFES/views/projects/under_ice_bt/samples/bt8/report/report.pdf ~/GEFES/reports/under_ice_bt/bt8.pdf
+
+rsync -av lsinclai@sisu-login1.csc.fi:/wrk/eiler/GEFES/views/projects/under_ice_lb/samples/lb1/report/report.pdf ~/GEFES/reports/under_ice_lb/lb1.pdf
+rsync -av lsinclai@sisu-login1.csc.fi:/wrk/eiler/GEFES/views/projects/under_ice_lb/samples/lb2/report/report.pdf ~/GEFES/reports/under_ice_lb/lb2.pdf
+rsync -av lsinclai@sisu-login1.csc.fi:/wrk/eiler/GEFES/views/projects/under_ice_lb/samples/lb3/report/report.pdf ~/GEFES/reports/under_ice_lb/lb3.pdf
+rsync -av lsinclai@sisu-login1.csc.fi:/wrk/eiler/GEFES/views/projects/under_ice_lb/samples/lb4/report/report.pdf ~/GEFES/reports/under_ice_lb/lb4.pdf
+rsync -av lsinclai@sisu-login1.csc.fi:/wrk/eiler/GEFES/views/projects/under_ice_lb/samples/lb5/report/report.pdf ~/GEFES/reports/under_ice_lb/lb5.pdf
+rsync -av lsinclai@sisu-login1.csc.fi:/wrk/eiler/GEFES/views/projects/under_ice_lb/samples/lb6/report/report.pdf ~/GEFES/reports/under_ice_lb/lb6.pdf
+rsync -av lsinclai@sisu-login1.csc.fi:/wrk/eiler/GEFES/views/projects/under_ice_lb/samples/lb7/report/report.pdf ~/GEFES/reports/under_ice_lb/lb7.pdf
+rsync -av lsinclai@sisu-login1.csc.fi:/wrk/eiler/GEFES/views/projects/under_ice_lb/samples/lb8/report/report.pdf ~/GEFES/reports/under_ice_lb/lb8.pdf
+
+rsync -av lsinclai@sisu-login1.csc.fi:/wrk/eiler/GEFES/views/projects/under_ice_kt/samples/kt1/report/report.pdf ~/GEFES/reports/under_ice_kt/kt1.pdf
+rsync -av lsinclai@sisu-login1.csc.fi:/wrk/eiler/GEFES/views/projects/under_ice_kt/samples/kt2/report/report.pdf ~/GEFES/reports/under_ice_kt/kt2.pdf
+rsync -av lsinclai@sisu-login1.csc.fi:/wrk/eiler/GEFES/views/projects/under_ice_kt/samples/kt3/report/report.pdf ~/GEFES/reports/under_ice_kt/kt3.pdf
+rsync -av lsinclai@sisu-login1.csc.fi:/wrk/eiler/GEFES/views/projects/under_ice_kt/samples/kt4/report/report.pdf ~/GEFES/reports/under_ice_kt/kt4.pdf
+rsync -av lsinclai@sisu-login1.csc.fi:/wrk/eiler/GEFES/views/projects/under_ice_kt/samples/kt5/report/report.pdf ~/GEFES/reports/under_ice_kt/kt5.pdf
+rsync -av lsinclai@sisu-login1.csc.fi:/wrk/eiler/GEFES/views/projects/under_ice_kt/samples/kt6/report/report.pdf ~/GEFES/reports/under_ice_kt/kt6.pdf
+rsync -av lsinclai@sisu-login1.csc.fi:/wrk/eiler/GEFES/views/projects/under_ice_kt/samples/kt7/report/report.pdf ~/GEFES/reports/under_ice_kt/kt7.pdf
+rsync -av lsinclai@sisu-login1.csc.fi:/wrk/eiler/GEFES/views/projects/under_ice_kt/samples/kt8/report/report.pdf ~/GEFES/reports/under_ice_kt/kt8.pdf
