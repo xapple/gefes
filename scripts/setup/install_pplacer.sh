c42335a7c0cbff02aa42b05687d8c68ec002f3b4
Lucas Sinclair <lucas.sinclair@me.com>
2015-12-09 14:26:06 +0100

    It's only one directory

--- scripts/setup/install_pplacer.sh
+++ scripts/setup/install_pplacer.sh
@@ -2,7 +2,5 @@ cd ~/test/
 wget https://github.com/matsen/pplacer/releases/download/v1.1.alpha17/pplacer-Linux-v1.1.alpha17.zip
 unzip pplacer-Linux-v1.1.alpha17.zip
 mv pplacer-Linux-v1.1.alpha17 $HOME/programs/pplacer17/
-export PATH="$HOME/programs/pplacer17/pplacer":$PATH
-export PATH="$HOME/programs/pplacer17/guppy":$PATH
-export PATH="$HOME/programs/pplacer17/rppr":$PATH
+export PATH="$HOME/programs/pplacer17":$PATH
 find $HOME/programs/pplacer17 -type f -print0 | xargs -0 chmod u-w
