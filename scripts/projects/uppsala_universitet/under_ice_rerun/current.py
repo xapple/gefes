5cc5b3cc89694a4f1d7fedd9cf77eb4b57cba938
Lucas Sinclair <lucas.sinclair@me.com>
2017-11-03 16:30:22 +0100

    Added JSON for under_ice_rerun

--- scripts/projects/uppsala_universitet/under_ice_rerun/current.py
+++ scripts/projects/uppsala_universitet/under_ice_rerun/current.py
@@ -8,12 +8,11 @@ A script to run small snippets of code on the under ice rerun project.
 import gefes, shutil
 
 # Three projects #
-bt = gefes.projects['under_ice_rerun_bt'].load()
-lb = gefes.projects['under_ice_rerun_lb'].load()
-kt = gefes.projects['under_ice_rerun_kt'].load()
-for s in bt.samples: s.load()
-for s in lb.samples: s.load()
-for s in kt.samples: s.load()
+bt = gefes.load("~/deploy/gefes/metadata/json/projects/uppsala_universitet/bt/")
+lb = gefes.load("~/deploy/gefes/metadata/json/projects/uppsala_universitet/lb/")
+kt = gefes.load("~/deploy/gefes/metadata/json/projects/uppsala_universitet/kt/")
+
+# Samples combined #
 samples = tuple(bt.samples + lb.samples + kt.samples)
 projects = (bt, lb, kt)
 
